33f9e5d8d4f64a707f8df0199a240a68b32b33ab
ashleefr <m2000140@edu.misis.ru>
2023-01-10 14:22:52 +0300

    test pow done

--- s21_pow.c
+++ s21_pow.c
@@ -4,11 +4,7 @@ long double s21_pow(double x, double y) {
     long double result = 0;
     int sign = x < 0 && s21_fmod(y, 2) == 1 ? -1 : 1;
     x = x < 0 ? -x : x;
-    if (y < 0) {
-        // y = -y;
-        x =  1 / x;
-    }
-    // printf("\t[DEBUG] x = %lf\ty = %lf\n", x, y);
+//    x = y < 0 ? 1 / x : x;
     result = s21_exp(y * (double) s21_log(x));
 
     return sign * result;

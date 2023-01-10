58e73d90163f587f5634a5bc922c8e89a50f354d
mitchell <okhlopkov.matvey@mail.ru>
2023-01-10 10:17:39 +0000

    update test

--- suite_s21_log.c
+++ suite_s21_log.c
@@ -13,7 +13,8 @@ START_TEST(test_log_3) {
 } END_TEST
 
 START_TEST(test_log_4) {
-    ck_assert_ldouble_eq_tol(s21_log(-1.), log(-1.), S21_CHECK);
+    ck_assert_ldouble_nan(s21_log(-1.));
+    ck_assert_ldouble_nan(log(-1.));
 } END_TEST
 
 START_TEST(test_log_5) {
@@ -21,7 +22,17 @@ START_TEST(test_log_5) {
 } END_TEST
 
 START_TEST(test_log_6) {
-    ck_assert_ldouble_eq_tol(s21_log(-0.5), log(-0.5), S21_CHECK);
+    ck_assert_ldouble_nan(s21_log(-0.5));
+    ck_assert_ldouble_nan(log(-0.5));
+} END_TEST
+
+START_TEST(test_log_7) {
+    ck_assert_ldouble_infinite(s21_log(S21_INF));
+    ck_assert_ldouble_infinite(log(S21_INF));
+} END_TEST
+
+START_TEST(test_log_8) {
+    ck_assert_ldouble_eq_tol(s21_log(0.0), log(0.0), S21_CHECK);
 } END_TEST
 
         Suite *suite_s21_log(void) {
@@ -37,6 +48,8 @@ START_TEST(test_log_6) {
     tcase_add_test(tc_log, test_log_4);
     tcase_add_test(tc_log, test_log_5);
     tcase_add_test(tc_log, test_log_6);
+    tcase_add_test(tc_log, test_log_7);
+    tcase_add_test(tc_log, test_log_8);
 
     suite_add_tcase(s, tc_log);
     return s;
--- suite_s21_pow.c
+++ suite_s21_pow.c
@@ -24,6 +24,14 @@ START_TEST(test_pow_6) {
     ck_assert_ldouble_eq_tol(s21_pow(-0.5, 5), pow(-0.5, 5), S21_CHECK);
 } END_TEST
 
+START_TEST(test_pow_7) {
+        ck_assert_ldouble_eq_tol(s21_pow(0.0, 1.0), pow(0.0, 1.0), S21_CHECK);
+} END_TEST
+
+START_TEST(test_pow_8) {
+        ck_assert_ldouble_eq_tol(s21_pow(10.0, -1.0), pow(10.0, -1.0), S21_CHECK);
+} END_TEST
+
 Suite *suite_s21_pow(void) {
     Suite *s;
     TCase *tc_pow;
@@ -37,6 +45,8 @@ Suite *suite_s21_pow(void) {
     tcase_add_test(tc_pow, test_pow_4);
     tcase_add_test(tc_pow, test_pow_5);
     tcase_add_test(tc_pow, test_pow_6);
+    tcase_add_test(tc_pow, test_pow_7);
+    tcase_add_test(tc_pow, test_pow_8);
 
     suite_add_tcase(s, tc_pow);
 return s;

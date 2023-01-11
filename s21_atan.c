aa71bfbabe47fae6737ca1c6e0375fd614e7fb38
mitchell <okhlopkov.matvey@mail.ru>
2023-01-11 14:30:04 +0000

    update cos atan pow

--- s21_atan.c
+++ s21_atan.c
@@ -13,15 +13,23 @@ long double s21_atan(double x){
             result += temp;
             x_degree *= x * x;
         }
-    } else {
+    } else if (x > 0){
         long double x_degree = 1 / x;
-        for (int n = 0; n < 7000; n++) {
-            temp = pow_integer(-1.0, n) * x_degree / (2 * n + 1);
+        for (int i = 0; i < 7000; i++) {
+            temp = s21_pow(-1, i) * x_degree / (2 * i + 1);
             result += temp;
             x_degree *= 1 / (x * x);
         }
         result = S21_PI / 2 - result;
-    }
+    } else {
+        double tmp = s21_fabs(x);
+        long double x_degree = 1 / tmp;
+        for (int i = 0; i < 7000; i++) {
+            temp = s21_pow(-1, i) * x_degree / (2 * i + 1);
+            result += temp;
+            x_degree *= 1 / (tmp * tmp);
+        }
+        result = - (S21_PI / 2 - result);}
      
     return result;
 }
--- s21_pow.c
+++ s21_pow.c
@@ -2,10 +2,15 @@
 
 long double s21_pow(double x, double y) {
     long double result = 0;
-    int sign = x < 0 && s21_fmod(y, 2) == 1 ? -1 : 1;
-    x = x < 0 ? -x : x;
-//    x = y < 0 ? 1 / x : x;
-    result = s21_exp(y * (double) s21_log(x));
+    int sign = x < 0 && s21_fmod(y, 2) != 0 ? -1 : 1;
+    if (x < 0 && s21_fmod(y, 1) != 0) {
+        result = S21_NAN;
+    } else {
+        x = x < 0 ? -x : x; 
+        //    x = y < 0 ? 1 / x : x;
+        result = s21_exp(y * (double) s21_log(x));
 
+    }
+    
     return sign * result;
 } // x^y = e^(y*ln(x))
